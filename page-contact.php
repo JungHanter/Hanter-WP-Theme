1669bcb38f7bf6385a016f6ba14a1f1624846345
Hanter Jung <hanterkr@gmail.com>
2015-01-02 12:18:35 +0900

    add pages

---
+++ page-contact.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hanter
+ * Date: 2015. 1. 2.
+ * Time: 오후 12:04
+ */
+get_header(); ?>
+
+    Contact
+
+<?php get_footer(); ?>
---
+++ page-home.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hanter
+ * Date: 2015. 1. 2.
+ * Time: 오후 12:04
+ */
+get_header(); ?>
+
+    Home
+
+<?php get_footer(); ?>
